85739850fa4af38723842efbde5f481c8083a3a5
Marvin Reza <marvinreza@outlook.com>
2017-01-05 02:44:53 +0100

    Added moving ground

--- core/src/com/marvin/game/states/PlayState.java
+++ core/src/com/marvin/game/states/PlayState.java
@@ -3,6 +3,7 @@ package com.marvin.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.marvin.game.FlappyBird;
 import com.marvin.game.sprites.Bird;
 import com.marvin.game.sprites.Tube;
@@ -16,9 +17,12 @@ import java.util.ArrayList;
 public class PlayState extends State {
     private static final int TUBE_SPACING = 125;
     private static final int TUBE_COUNT = 4;
+    private static final int GROUND_Y_OFFSET = -50;
 
     private Bird bird;
     private Texture background;
+    private Texture ground;
+    private Vector2 groundPos1, groundPos2;
 
     private ArrayList<Tube> tubes;
 
@@ -27,6 +31,9 @@ public class PlayState extends State {
         bird = new Bird(50,300);
         cam.setToOrtho(false, FlappyBird.WIDTH / 2,FlappyBird.HEIGHT / 2);
         background = new Texture("bg.png");
+        ground = new Texture("ground.png");
+        groundPos1 = new Vector2(cam.position.x - (cam.viewportWidth / 2), GROUND_Y_OFFSET);
+        groundPos2 = new Vector2((cam.position.x - (cam.viewportWidth / 2))+ ground.getWidth(), GROUND_Y_OFFSET);
 
         tubes = new ArrayList<Tube>();
 
@@ -44,6 +51,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateGround();
         bird.update(dt);
         cam.position.x = bird.getPosition().x + 80;
 
@@ -57,7 +65,8 @@ public class PlayState extends State {
             if (tube.collides(bird.getBounds()))
                 gsm.set(new PlayState(gsm));
         }
-
+        if(bird.getPosition().y  <= ground.getHeight() + GROUND_Y_OFFSET)
+            gsm.set(new PlayState(gsm));
         cam.update();
     }
 
@@ -73,6 +82,8 @@ public class PlayState extends State {
             sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
         }
 
+        sb.draw(ground, groundPos1.x, groundPos1.y);
+        sb.draw(ground, groundPos2.x, groundPos2.y);
         sb.end();
     }
 
@@ -82,7 +93,14 @@ public class PlayState extends State {
         bird.dispose();
         for(Tube tube: tubes)
             tube.dispose();
+        ground.dispose();
         System.out.println("Play State disposed!");
+    }
 
+    public void updateGround() {
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPos1.x + ground.getWidth())
+            groundPos1.add(ground.getWidth() * 2, 0 );
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPos2.x + ground.getWidth())
+            groundPos2.add(ground.getWidth() * 2, 0 );
     }
 }
